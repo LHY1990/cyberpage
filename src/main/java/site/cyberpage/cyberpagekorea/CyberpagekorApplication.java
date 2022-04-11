9000691098daf95d59bf371589a124e05a39afb1
LHY1990 <gnsdud3163@gmail.com>
2022-04-11 20:25:19 +0900

    Feat : 그래들 설정 및 의존성 주입

---
+++ src/main/java/site/cyberpage/cyberpagekorea/CyberpagekorApplication.java
@@ -0,0 +1,13 @@
+package site.cyberpage.cyberpagekorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberpagekorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberpagekorApplication.class, args);
+	}
+
+}
